0e43906b1bd06d3570b4f5c24c4134ac6dc45daf
duzhongbo <710409599@qq.com>
2015-12-13 20:57:54 +0800

    代码整理，加注释

--- _includes/view/index.js
+++ _includes/view/index.js
@@ -2,7 +2,7 @@ var vIndex = Backbone.View.extend({
 	initialize : function () {
 		this.renderArticleList();
 	},
-	renderArticleList:function (model) {
+	renderArticleList:function (model) {// 渲染文章列表
 		$('.index-article-ul').html(this.template(this.model.toJSON()));
 	},
 	template:_.template($('#template').html()),
@@ -11,18 +11,21 @@ var vIndex = Backbone.View.extend({
 		'click .index-article-a':'showDetail',
 		'click .index-title':'changeBg'
 	},
-	showDetail:function (e) {
+	showDetail:function (e) {// 改变hash值，触发路由显示相应的页面
 		var target = e.target || e.srcElement;
 		var url = $(target).attr('href');
 		location.hash='article'+url;
 		return false;
 	},
 	aBg:['bg0','bg1','bg2'],
-	changeBg:function () {
+	changeBg:function () {// 切换首页的背景
 		var aBg = this.aBg;
 		var sOld  = aBg[0];
 		var first= aBg.shift();
 		aBg.push(first);
 		$('.wrap').addClass(aBg[0]).removeClass(sOld);
+	},
+	nextPage:function (nStart) {// 翻页
+		
 	}
 });
