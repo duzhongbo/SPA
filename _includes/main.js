79253c6f05d6b3fecf29c23ab1475ddb3a6770f7
duzhongbo <710409599@qq.com>
2015-12-14 15:18:01 +0800

    排错，还原到首页视图

--- _includes/main.js
+++ _includes/main.js
@@ -3,9 +3,26 @@
 // 博客全部数据
 {% include data.js %}
 $(function () {
+	// 导航视图
+	{% include view/Nav.js %}
+	var nav = new Nav;
 	
+	// 路由与历史管理
+	{% include router/Router.js %}	
+	var router = new Router;
+	Backbone.history.start();
 	
+	// 文章列表模型
+	var Model = Backbone.Model.extend({
+		defaults : {
+			data : aData.article
+		}
+	});
+	var model = new Model;
 
+	// 首页视图
+	{% include view/Index.js %}
+	var index = new Index({model:model});
 
 
 
