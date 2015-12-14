8917f4f40c3b3daa8102f8efb3695b90a17a65da
duzhongbo <710409599@qq.com>
2015-12-14 15:49:09 +0800

    还原主代码中的jekyll引用

--- _includes/main.js
+++ _includes/main.js
@@ -4,8 +4,29 @@
 {% include data.js %}
 $(function () {
 	// 导航视图
-	{% include view/abc1.js %}
+	{% include view/nav.js %}
+	var nav = new Nav;
 	
+	// 路由与历史管理
+	{% include router/router.js %}	
+	var router = new Router;
+	Backbone.history.start();
+	
+	// 文章列表模型
+	var Model = Backbone.Model.extend({
+		defaults : {
+			data : aData.article
+		}
+	});
+	var model = new Model;
+
+	// 首页视图
+	{% include view/index.js %}
+	var index = new Index({model:model});
+
+	// 列表页视图
+	{% include view/list.js %}
+	var list = new List({model:model});	
 
 
 
