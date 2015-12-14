8cf69ffa3409a932d030c47eda998fe8ee4b4137
duzhongbo <710409599@qq.com>
2015-12-14 14:59:56 +0800

    给列表页绑定点击进入文章页

--- _includes/view/list.js
+++ _includes/view/list.js
@@ -8,5 +8,15 @@ var List = Backbone.View.extend({
 		$('.list-ul').html(this.template(oData));
 	},
 	template:_.template($('#tplArticles').html()),
+	el:$('.list-ul'),
+	events:{
+		'click .list-li-a':'showDetail'
+	},
+	showDetail:function (e) {
+		var target = e.target || e.srcElement;
+		var url = $(target).attr('href');
+		location.hash='article'+url;
+		return false;
+	}
 
 })
