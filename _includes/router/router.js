b13ef7e844d5603975776b585d578cede2ebdb6a
duzhongbo <710409599@qq.com>
2015-12-14 20:08:44 +0800

    点击列表进入文章页事件从路由改到视图

--- _includes/router/router.js
+++ _includes/router/router.js
@@ -1,24 +1,8 @@
 var Router = Backbone.Router.extend({
 	routes: {
 		'':'index',// 只有域名的情况,首页
-		'article/:year/:month/:date/:title':'showDetail',// #article/kiwis，文章详情页
 		'list':'list',// 列表页
 		'tags':'tags'// 标签页
-	},
-	showDetail:function (year,month,date,title) {
-		var url = '/'+year+'/'+month+'/'+date+'/'+title,temp1,temp2;
-		for(var i=0,data=aArticle,len=data.length;i<len;i++){
-			temp1 = data[i];
-			temp2 = decodeURI(temp1.url);
-			if(temp2==url){
-				$('.article-title').html(temp1.title);
-				$('.article-content').html(this.html_decode(temp1.content));
-				$('.article-date').html(temp1.date);
-				$('.page').hide();
-				$('.article').show();
-				$('.index').attr('class','index cover oh page');
-			}
-		}
 	},
 	index:function () {
 		$('.page').hide();
@@ -32,18 +16,6 @@ var Router = Backbone.Router.extend({
 	tags:function () {
 		$('.page').hide();
 		$('.tags').show();
-	},
-	html_decode:function (str) {// 将已经转义的html标签重新转回
-		var s = "";   
-		if (str.length == 0) return "";   
-		s = str.replace(/&gt;/g, ">");   
-		s = s.replace(/&lt;/g, "<");   
-		s = s.replace(/&nbsp;/g, " ");   
-		s = s.replace(/&quot;/g, "\"");   
-		s = s.replace(/<br>/g, "\n");
-		s = s.replace(/class/g,' class');
-		s = s.replace(/src/g,' src');
-		return s;   
 	},
 	changeBg:function () {
 		var nBg = getCookie('nBg');
--- _includes/view/index.js
+++ _includes/view/index.js
@@ -30,7 +30,34 @@ var Index = Backbone.View.extend({
 		var target = e.target || e.srcElement;
 		var url = $(target).attr('href');
 		location.hash='article'+url;
+		this.filterData(url);
 		return false;
+	},
+	filterData:function (sUrl) {
+		for(var i=0,data=this.aData,len=data.length;i<len;i++){
+			temp1 = data[i];
+			temp2 = decodeURI(temp1.url);
+			if(temp2==sUrl){
+				$('.article-title').html(temp1.title);
+				$('.article-content').html(this.html_decode(temp1.content));
+				$('.article-date').html(temp1.date);
+				$('.page').hide();
+				$('.article').show();
+				$('.index').attr('class','index cover oh page');
+			}
+		}		
+	},
+	html_decode:function (str) {// 将已经转义的html标签重新转回
+		var s = "";   
+		if (str.length == 0) return "";   
+		s = str.replace(/&gt;/g, ">");   
+		s = s.replace(/&lt;/g, "<");   
+		s = s.replace(/&nbsp;/g, " ");   
+		s = s.replace(/&quot;/g, "\"");   
+		s = s.replace(/<br>/g, "\n");
+		s = s.replace(/class/g,' class');
+		s = s.replace(/src/g,' src');
+		return s;   
 	},
 	page:1,
 	nextPage:function () {// 翻页
