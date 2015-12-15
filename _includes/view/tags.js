756ad66e995cb78c5a152ccaf00910db63e069cd
duzhongbo <710409599@qq.com>
2015-12-15 11:50:28 +0800

    点击标签页文章列表进入文章页

--- _includes/view/tags.js
+++ _includes/view/tags.js
@@ -12,19 +12,30 @@ var Tags = Backbone.View.extend({
 		var target = e.target || e.srcElement;
 		var url = $(target).attr('href');
 		var tag = url.split('/tag=')[1];
-		var aArticle = aData[tag];
+		var aArticleTitle = aData[tag];
+		// 文章列表子模型
 		var Model = Backbone.Model.extend({
 			defaults : {
-				article : aArticle,
+				articleTitle : aArticleTitle,
+				article:aArticle
 			}
 		});
 		var model = new Model;
+		// 文章列表子视图
 		var TagsArticle = Backbone.View.extend({
 			initialize:function () {
 				var oData = this.model.toJSON();
+				this.aData = oData.article;
 				$('.tags-articles-ul').html(this.template(oData));
 			},
-			template:_.template($('#tplTagArticles').html())
+			template:_.template($('#tplTagArticles').html()),
+			el:$('.tags-articles-ul'),
+			events:{
+				'click .tags-articles-a':'showDetail'
+			},
+			showDetail:function (e) {
+				return com.showDetail(e,this.aData);
+			}
 		});
 		var tagsArticle = new TagsArticle({model:model});
 
