19ad51cd1382a73f1f5cc6455b4faf9c157dfdee
duzhongbo <710409599@qq.com>
2015-12-15 14:19:10 +0800

    搜索视图绑定事件

--- _includes/view/search.js
+++ _includes/view/search.js
@@ -1,9 +1,23 @@
 var Search = Backbone.View.extend({
 	el:$('.search'),
 	events:{
-		'click .search-bt':'submitSearch'
+		'click .search-bt':'click',
+		'blur .search-it':'blur',
+		'focus .search-it':'focus',
 	},
-	submitSearch:function () {
-		console.log('search!')
+	click:function () {
+		console.log('click');
+	},
+	blur:function () {
+		var val = $('.search-it').val();
+		if(val==''){
+			$('.search-it').val('搜索');
+		}
+	},
+	focus:function () {
+		var val = $('.search-it').val();
+		if(val=='搜索'){
+			$('.search-it').val('');
+		}
 	}
 });
