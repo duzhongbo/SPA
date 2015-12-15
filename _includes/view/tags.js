21f234683d55519b029a6e830ed11fc38088e2db
duzhongbo <710409599@qq.com>
2015-12-15 10:35:01 +0800

    给标签绑定点击事件

--- _includes/view/tags.js
+++ _includes/view/tags.js
@@ -4,4 +4,14 @@ var Tags = Backbone.View.extend({
 		$('.tags-ul').html(this.template(oData));
 	},
 	template:_.template($('#tplTags').html()),
+	el:$('.tags'),
+	events:{
+		'click .tags-a':'showArticles'
+	},
+	showArticles:function (e) {
+		var target = e.target || e.srcElement;
+		var url = $(target).attr('href');
+		console.log(url);
+		return false;
+	}
 });
