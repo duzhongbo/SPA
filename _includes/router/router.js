f73e931fe5c7a1644441a442fcd2976e221a3769
duzhongbo <710409599@qq.com>
2015-12-15 10:13:23 +0800

    路由增加文章页显示，防止刷新后文章页空白

--- _includes/router/router.js
+++ _includes/router/router.js
@@ -2,7 +2,8 @@ var Router = Backbone.Router.extend({
 	routes: {
 		'':'index',// 只有域名的情况,首页
 		'list':'list',// 列表页
-		'tags':'tags'// 标签页
+		'tags':'tags',// 标签页
+		'article/:year/:month/:date/:title':'showDetail',// #article/2015/11/05/xxoo.html文章页
 	},
 	index:function () {
 		$('.page').hide();
@@ -24,6 +25,10 @@ var Router = Backbone.Router.extend({
 		}
 		var cls = 'index-bg'+nBg
 		$('.index').addClass(cls);
+	},
+	showDetail:function () {
+		var url = (location.hash).split('#article')[1];
+		com.filterData(url,aArticle);
 	}
 });
 
